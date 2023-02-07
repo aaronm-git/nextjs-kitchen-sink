5d5ee15ac05028761835e1d979b317fc71a57562
Aaron Molina <28906210+aaronm-git@users.noreply.github.com>
2023-02-07 14:41:30 -0500

    tailwind configured

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,14 @@
-import "@/styles/globals.css";
+import { Global } from "@emotion/react";
+import xw from "xwind";
 import Layout from "@/components/Layout";
 
 export default function App({ Component, pageProps }) {
   return (
     <Layout>
+      <Global
+        //tailwind base styles + keyframes + ring and shadow classes variables  ... to global styles
+        styles={xw`XWIND_BASE XWIND_GLOBAL`}
+      />
       <Component {...pageProps} />
     </Layout>
   );
